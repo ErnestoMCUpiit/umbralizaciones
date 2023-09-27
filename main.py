0c680cdd6c95b7e2a9d0907edfd01433040934fd
Ernesto <emendietac2100@alumno.ipn.mx>
2023-09-26 20:35:45 -0600

    modificacion del main

--- main.py
+++ main.py
@@ -1,15 +1,31 @@
-import cv2  # Asegúrate de tener OpenCV instalado
+import cv2  
 import numpy as np
 from bersem import bernsen
 import matplotlib.pyplot as plt
 
-# Cargar la imagen
-imagen_original = cv2.imread("imagen.png", cv2.IMREAD_GRAYSCALE)
+celula = cv2.imread("image-cell.png", cv2.IMREAD_GRAYSCALE)
+bacteria = cv2.imread("person_bacteria.jpeg", cv2.IMREAD_GRAYSCALE)
+zebra = cv2.imread("zebra.jpg", cv2.IMREAD_GRAYSCALE)
 
-# Aplicar el algoritmo de umbralización de Berntsen
 imagen_umbralizada = bernsen(imagen_original, cmin=10, n=3, bg="oscuro")
+plt.figure(figsize=(15, 5))
 
-# Guardar la imagen umbralizada
+plt.subplot(331) 
+plt.imshow(celula)
+plt.title('original')
+
+plt.subplot(332)  
+plt.imshow(cv2.cvtColor(imagen2, cv2.COLOR_BGR2RGB))
+plt.title('Global')
+
+plt.subplot(333)  
+plt.imshow(cv2.cvtColor(imagen3, cv2.COLOR_BGR2RGB))
+plt.title('Otsu')
+
+plt.subplot(334)
+plt.imshow(bernsen(celula, cmin=10, n=3, bg="claro"))
+plt.title('local adaptativa por el método de Bernsen')
+
 cv2.imshow("imagen_umbralizada", imagen_umbralizada)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
