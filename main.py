edec58d7f5bc19a365ba1f5a367cfc6beb1aeca7
Ernesto <emendietac2100@alumno.ipn.mx>
2023-09-26 20:52:29 -0600

    ploteo de imagenes

--- main.py
+++ main.py
@@ -7,25 +7,63 @@ celula = cv2.imread("image-cell.png", cv2.IMREAD_GRAYSCALE)
 bacteria = cv2.imread("person_bacteria.jpeg", cv2.IMREAD_GRAYSCALE)
 zebra = cv2.imread("zebra.jpg", cv2.IMREAD_GRAYSCALE)
 
-imagen_umbralizada = bernsen(imagen_original, cmin=10, n=3, bg="oscuro")
+bersen1 = bernsen(celula, cmin=20, n=11, bg="claro")
+bersen2 = bernsen(bacteria, cmin=10, n=3, bg="oscuro")
+bersen3 = bernsen(zebra, cmin=10, n=3, bg="oscuro")
+
 plt.figure(figsize=(15, 5))
 
-plt.subplot(331) 
+# imagenes procesadas celula
+plt.subplot(341) 
 plt.imshow(celula)
 plt.title('original')
 
-plt.subplot(332)  
-plt.imshow(cv2.cvtColor(imagen2, cv2.COLOR_BGR2RGB))
+plt.subplot(342)  
+plt.imshow(celula)
 plt.title('Global')
 
-plt.subplot(333)  
-plt.imshow(cv2.cvtColor(imagen3, cv2.COLOR_BGR2RGB))
+plt.subplot(343)  
+plt.imshow(celula)
 plt.title('Otsu')
 
-plt.subplot(334)
-plt.imshow(bernsen(celula, cmin=10, n=3, bg="claro"))
+plt.subplot(344)
+plt.imshow(bersen1,cmap="gray")
 plt.title('local adaptativa por el método de Bernsen')
 
-cv2.imshow("imagen_umbralizada", imagen_umbralizada)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+#imagenes procesadas bacteria
+plt.subplot(345) 
+plt.imshow(bacteria)
+plt.title('original')
+
+plt.subplot(346)  
+plt.imshow(bacteria)
+plt.title('Global')
+
+plt.subplot(347)  
+plt.imshow(bacteria)
+plt.title('Otsu')
+
+plt.subplot(348)
+plt.imshow(bersen2,cmap="gray")
+plt.title('local adaptativa por el método de Bernsen')
+
+#imagenes procesadas paso de cebra
+plt.subplot(349) 
+plt.imshow(zebra)
+plt.title('original')
+
+plt.subplot(3,4 ,10)  
+plt.imshow(zebra)
+plt.title('Global')
+
+plt.subplot(3,4,11)  
+plt.imshow(zebra)
+plt.title('Otsu')
+
+plt.subplot(3,4,12)
+plt.imshow(bersen3, cmap="gray")
+plt.title('local adaptativa por el método de Bernsen')
+
+plt.show()
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
